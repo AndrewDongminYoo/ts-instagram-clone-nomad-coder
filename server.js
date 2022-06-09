d4a461a42588ef61a47f83a38fb18fb744484eac
ydm2790 <ydm2790@gmail.com>
2022-06-09 13:34:22 +0900

    feat: quries

--- server.js
+++ server.js
@@ -2,17 +2,40 @@ import { ApolloServer, gql } from 'apollo-server';
 
 // The GraphQL schema
 const typeDefs = gql`
+
+  type Movie {
+    title: String
+    year: Int
+  }
+
   type Query {
     "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2022 })
   },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: (_, args) => {
+      console.log(args);
+      return true;
+    }
+  }
 };
 
 const server = new ApolloServer({
