69e874a26e75a72863828c5f46765238accdab29
ydm2790 <ydm2790@gmail.com>
2022-06-11 17:12:57 +0900

    feat: logger

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { resolvers, typeDefs } from './src/schema';
 import { protectResolver, verifyToken } from './src/users/user.utils';
 import { Secret } from 'jsonwebtoken';
+import logger from 'morgan';
 import { graphqlUploadExpress } from "graphql-upload";
+import express from 'express';
 
 declare global {
   namespace NodeJS {
@@ -14,24 +16,28 @@ declare global {
     }
   }
 }
+(async function () {
+  const server: ApolloServer = new ApolloServer({
+    resolvers,
+    typeDefs,
+    context: async ({ req }) => {
+      const token = req.headers.authorization || '';
+      return {
+        activeUser: await (token ? verifyToken(token) : null),
+        protectResolver,
+      };
+    },
+    // Using graphql-upload without CSRF prevention is very insecure.
+    csrfPrevention: true,
+  });
 
-const server: ApolloServer = new ApolloServer({
-  cors: true,
-  resolvers,
-  typeDefs,
-  context: async ({ req }) => {
-    const token = req.headers.authorization || '';
-    return {
-      activeUser: await (token ? verifyToken(token) : null),
-      protectResolver,
-    };
-  },
-  // Using graphql-upload without CSRF prevention is very insecure.
-  csrfPrevention: true,
-});
+  await server.start();
+  const PORT = process.env.PORT;
+  const app = express();
+  app.use(logger('tiny'));
+  server.applyMiddleware({ app });
 
-const PORT = process.env.PORT;
-
-server.listen(PORT).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-})
+  app.listen({ port: PORT }, () => {
+    console.log(`🚀  Server is running on http://localhost:${PORT}`);
+  })
+})()
